315a067bc0f4b4e459d40234ad62bdcaaee01ee1
Amit Bhatia <amit_bhatia@colpal.com>
2022-05-25 07:01:10 -0400

    Find missing number

--- algorithms/missing_element.c
+++ algorithms/missing_element.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 #include <memory.h>
 
-// SOLUTION
+int log_print = 0;
 
 /* getMissingNo takes array and size of array as arguments*/
 int getMissingNo(int a[], int n)
 {
-    int i, total;
-    total = (n + 1) * (n + 2) / 2;
-    for (i = 0; i < n; i++)
-        total -= a[i];
+    int i=0, total;
+    total = 1;
+    for (i = 2; i <=n; i++) {
+        total += i;
+        total -= a[i-2];
+        if(log_print) printf("%02d: total(%2d), a[%d]=(%d)\n",i,total,i-2,a[i-2]);       
+    }
     return total;
 }
 
@@ -21,58 +24,7 @@ int main(int argc, char * argv[])
     //int N = 5;
     //int A[] = {1,2,3,5};
     int N = 10;
-    int A[] = {6,1,2,8,3,4,9,10,5};
-
-    int t=A[0];
-    int m;
-
-    // Move the extra to the end
-    int i =0;
-
-    int out_of_bound;
-    while(i<N-2) {
-        if(A[i]>=N) {
-            out_of_bound=A[i];
-            A[i]=A[N-1];
-            A[N-1]=out_of_bound;            
-        }
-
-    }
-
-
-    while(i<N-1) {
-        printf("[%d,%d]: ", i,t);
-        for (int j=0; j<N;j++) {    
-            printf("%d ", A[j]);
-        } 
-        printf("\n");
-        if(A[i]>=N) {
-            //swap(A[N-1], A[i]);
-            t=A[N-1];
-            A[N-1]=A[i];
-            A[i]=t;
-        } 
-        else if (A[i] == i) {
-            i++;
-        }
-        else {
-            //swap(A[i], A[A[i]]);
-            if(A[A[i]] >= N) {
-                t=A[A[i]-1];
-                A[A[i]-1]=A[i];
-                A[i]=t;
-            } else {
-                t=A[A[i]];
-                A[A[i]]=A[i];
-                A[i]=t;
-            }
-        }
-        getchar();
-    }
-    for (int i=0; i<N;i++) {    
-        if(A[i] != i) {
-            printf("The missing # is %d\n", i);
-        }
-    } 
+    int A[] = {6,1,2,8,3,4,9,10,5,11};
+    printf("The missing number is %d\n", getMissingNo(A, N));
     
 }
