b968af189bb2f13e47b38b840cb2c890969e3df2
Amit Bhatia <amit_bhatia@colpal.com>
2022-05-31 11:39:46 -0400

    clear code

--- containers/my_rand.cpp
+++ containers/my_rand.cpp
@@ -10,9 +10,6 @@
 #include <string>
 #include <climits>
 
-
-//#include <GetOpt.h>
-
 using namespace std;
 
 int get_seed()
@@ -32,6 +29,10 @@ class MainParam{
     uint64_t low;
     uint64_t high;
     uint64_t random_seed;
+    uint64_t N;
+    string seprator;
+    bool debug;
+    bool verbose;
     string filename; 
     
     MainParam()
@@ -40,24 +41,29 @@ class MainParam{
         low=0;
         high=UINT64_MAX;
         random_seed = get_seed();
+        seprator = '\n';
+        filename = "random100.txt";
+        N = 100;
+        verbose =false;
+        debug = false;
     }
     ~MainParam() 
     {
         initialized = false;
     };
 };
-MainParam P;
+MainParam P;        
 
 static inline void print_help()
 {
     cout << "Require some options to run \n";
-    cout << "\t low     :   required_argument, \n";
-    cout << "\t high    :   required_argument,\n";      
-    cout << "\t MaxNbr  :   required_argument,\n";
-    cout << "\t file    :   required_argument, \n";     
-    cout << "\t debug   :   no_argument,\n";
-    cout << "\t verbose :   no_argument,\n";
-    cout << "\t separator :   optional_argument (default is newline)" << endl; 
+    cout << "\t low         :   lowest random, \n";
+    cout << "\t max         :   highest random number,\n";      
+    cout << "\t nbr         :   Number of random elements to be printed in the file ,\n";
+    cout << "\t file        :   name of the file to store random numbers, \n";     
+    cout << "\t debug       :   enable debug mode,\n";
+    cout << "\t verbose     :   print random numbers being generated on screen as well,\n";
+    cout << "\t separator   :   optional_argument (default is newline)" << endl; 
 }
 static inline int local_process_opt(int argc, char **argv)
 {
@@ -67,107 +73,91 @@ static inline int local_process_opt(int argc, char **argv)
     }  
     int c;
     int digit_optind = 0; 
+    int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"low",     required_argument, 0,  'l' },
+        {"max",     required_argument, 0,  'm' },
+        {"nbr",     required_argument, 0,  'n' },
+        {"debug",   no_argument,       0,  'd' },
+        {"file",    required_argument, 0,  'f' },
+        {"seprator", optional_argument, 0,  's' },        
+        {"help",    no_argument,           0, 'h' },        
+        {"verbose", no_argument,        0,  'v' },        
+        {0,         0,                 0,  0 }
+    };
     while (1) {
-        int this_option_optind = optind ? optind : 1;
-        int option_index = 0;
-        static struct option long_options[] = {
-            {"low",     required_argument, 0,  'l' },
-            {"high",    required_argument, 0,  'h' },
-            {"MaxNbr",  required_argument, 0,  'm' },
-            {"debug",   no_argument,       0,  'd' },
-            {"file",    required_argument, 0,  'f' },
-            {"seprator", optional_argument, 0,  's' },        
-            {"verbose", no_argument,       0,  'v' },        
-            {0,         0,                 0,  0 }
-        };
-
-        c = getopt_long(argc, argv, "l:h:m:f:df:s::v?",
+        c = getopt_long(argc, argv, "l:m:n:f:dhs::v?",
                 long_options, &option_index);
         if (c == -1) {
             break;
         }
+    
         switch (c) {
             case 'l':
-                printf("option c with value '%s'\n", optarg);
-                break;
-            case 'h':
-                printf("option '%c' with value '%s'\n", c, optarg);
+                P.low=atoi(optarg);
                 break;
             case 'm':
-                printf("option c with value '%s'\n", optarg);
+                P.high=atoi(optarg);
                 break;
-            case '?':
+            case 'n':
+                P.N=atoi(optarg);
+                break;
+            case 's':
+                //printf("option '%c' with value '%s'\n", c, optarg);
+                P.seprator=optarg;
+                cout << "{ P.seprator : \"" << P.seprator << "\" }" << endl;
+                break;
+            case 'd':
+                P.debug=true;
+                break;
+            case 'v':
+                P.verbose=true;
+                break;
+            case 'f':
+                P.filename=optarg;
+                break;
+            case 'h':
                 print_help();
                 break;
             default:
                 printf("default values for option '%c'", c);
+                cout << "c = " << c << "& optarg" << optarg << endl;
                 break;
         } // switch (c) 
-        if (optind < argc) {
-            printf("non-option ARGV-elements: ");
-            while (optind < argc)
-                printf("%s ", argv[optind++]);
-            printf("\n");
-        }         
-
     }
+    if (optind < argc) {
+        printf("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf("%s ", argv[optind++]);
+        printf("\n");
+    }         
     return EXIT_SUCCESS;
 }
 
 int main (int argc, char *argv[])
 {
-    int iSecret, iGuess;
     ofstream myfile;
-    long long MaxNbr = 99999;
-    long low=0;
-    long high=LLONG_MAX;
 
-    // process_input
-
-#if 1
+    if(argc == 2 && argv[1][0] == '?') {
+        print_help();
+        return EXIT_SUCCESS;
+    }
     if (local_process_opt(argc, argv) == EXIT_FAILURE) {
         return EXIT_SUCCESS;
     } 
-    /* initialize random seed: */
+    /* initialize random seed: TBD add more seed option later */
     srand (time(NULL));
-    myfile.open(filename);
-    /* generate secret number between 1 and 10: */
-    for(long i=0; i<MaxNbr; i++) {
-        myfile << rand() % 99999 + 1 << endl;
-    }    
-#else
-    if(argc > 2){
-        string value = argv[1];
-        string::size_type sz = 0;
-        MaxNbr =  stoll(value,&sz,0);
-        cout << value.substr(0,sz) << " interpreted as " << MaxNbr << '\n';
-        value = argv[2];
-        
-        if (value.find(".txt") != (value.length() - 4))
-            value.append(".txt");
-        cout << value << endl;
-        myfile.open(value); 
-        if(argc == 5) {
-            value = argv[3];
-            low = stoll(value,&sz,0);
-            cout << value.substr(0,sz) << " interpreted as " << low << endl;
-            value = argv[4];
-            high = stoll(value,&sz,0);
-            cout << value.substr(0,sz) << " interpreted as " << low << endl; 
+
+    myfile.open(P.filename);
+    uint64_t value = 0;
+    for(uint64_t i=0; i<P.N; i++) {
+        value = P.low + rand() % (P.high - P.low);
+        if(P.verbose) {
+            cout << value << P.seprator;
         }
-
-    } else {
-        myfile.open("input_100.txt");
-    }
-    /* initialize random seed: */
-    srand (time(NULL));
-
-    /* generate secret number between 1 and 10: */
-    
-    for(long i=0; i<MaxNbr; i++) {
-        myfile << rand() % 99999 + 1 << endl;
-    }
-#endif
-
+        myfile <<  value << P.seprator;
+    }    
+    myfile << endl;     
     return 0;
 }
